e394b0500571b5862aef7115d6a7042bf5f48fd6
nomorezoe <zoewangwork@gmail.com>
2024-01-12 21:21:12 +0800

    d

--- task_render.js
+++ task_render.js
@@ -96,6 +96,10 @@ function openpose_controlnet(bytes, strength) {
     return json;
 }
 
+function use_lora(model, weight){
+    return "<lora:{"+model+"}:{"+weight +"}>";
+}
+
 function TaskRender(task, req, queue) {
     console.log('TaskRender')
     var rawImg = req.files.imageByteArray.data;
@@ -138,7 +142,7 @@ function TaskRender(task, req, queue) {
     var json = {
         "model": model,  // can be any of the models in ["realismEngineSDXL_v20VAE", "realisticVisionV60B1_v60B1VAE", "Deliberate_v5", "dreamshaper_8", "dynavisionXLAllInOneStylized_release0557Bakedvae"]
         "clip_skip": 1,
-        "prompt": prompt,
+        "prompt": prompt +  ", " + use_lora("SDXL1.0_Essenz-series-by-AI_Characters_Style_BetterPhotography-v1.2-Skynet", 1.5) + ", " + use_lora("Cinematic_Hollywood_Film_sdxl", 1.0),
         "negative_prompt": negtext,//"(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3) horror",
         "seed": Tool.randomInt(999999999),
         "width": 768,
