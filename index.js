457a5d1063f48a54ec60f5b28b9be5525b0d7bd2
nomorezoe <zoewangwork@gmail.com>
2024-01-17 09:32:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const https = require('https');
 const server = https.createServer({
-    key: fs.readFileSync("./certs/generated-private-key.pem", 'utf-8'),
-    cert: fs.readFileSync("./certs/218a60880d642302.crt", 'utf-8'),
+    key: fs.readFileSync("./certs/server.key", 'utf-8'),
+    cert: fs.readFileSync("./certs/server.cert", 'utf-8'),
 }, app);
 const { Server } = require("socket.io");
 const io = new Server(server);
