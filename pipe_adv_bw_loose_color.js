f8650a310c6779cb672a550d53555b062780d4c7
nomorezoe <zoewangwork@gmail.com>
2024-03-06 16:09:04 +0800

    update

--- pipe_adv_bw_loose_color.js
+++ pipe_adv_bw_loose_color.js
@@ -11,16 +11,16 @@ function PipeAdvanceBWLooseColor() {
 PipeAdvanceBWLooseColor.process = function(imgData, positivePrompt, negtivePrompt, modelFile, loras, style, cfg, sampleSteps, sampler, scheduler, poseStrength, depthStrength, isLockCharacter, characterFile, fullCharacterPath){
 
     console.log("PipeAdvanceBWLooseColor");
-    const promptFile = fs.readFileSync('./pipe/workflow_api_adv_bw_loose_color.json');//');
+    const promptFile = fs.readFileSync('./pipe/workflow_api_adv_bw_loose_color_2.json');//');
     let prompt = JSON.parse(promptFile);
 
-    prompt["1"]["inputs"]["image"]=imgData;
+    prompt["44"]["inputs"]["image"]=imgData;
 
-    prompt["55"]["inputs"]["text_positive"] = positivePrompt;
+    prompt["6"]["inputs"]["text"] = positivePrompt;
     //prompt["55"]["inputs"]["text_negative"] = negtivePrompt;
     //prompt["55"]["inputs"]["style"] = style;
 
-    prompt["45"]["inputs"]["seed"] =  Tool.randomInt();
+    prompt["3"]["inputs"]["seed"] =  Tool.randomInt();
 
     return prompt;
 }
