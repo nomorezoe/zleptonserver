54b5a8d708e9c9a53be1605cca8c72ce0f25b8c7
nomorezoe <zoewangwork@gmail.com>
2024-03-06 15:31:02 +0800

    update

--- pipe_adv_loose_color.js
+++ pipe_adv_loose_color.js
@@ -11,26 +11,24 @@ function PipeAdvanceLooseColor() {
 PipeAdvanceLooseColor.process = function(imgData, positivePrompt, negtivePrompt, modelFile, loras, style, cfg, sampleSteps, sampler, scheduler, poseStrength, depthStrength, isLockCharacter, characterFile, fullCharacterPath){
 
     console.log("PipeAdvanceLooseColor");
-    const promptFile = fs.readFileSync('./pipe/workflow_api_adv_loose_color.json');//');
+    const promptFile = fs.readFileSync('./pipe/workflow_api_adv_loose_color_2.json');//');
     let prompt = JSON.parse(promptFile);
 
 
-    let tailText = "digital artwork, illustrative, painterly, matte painting, highly detailed";
-    let tailTexts = tailText.split(",");
-    for(let i in tailTexts){
-        if(positivePrompt.indexOf(tailTexts[i]) == -1){
-            positivePrompt += " "+ tailTexts[i] +",";
-        }
-    }
+    let tailText = ",Warm color temperature, serene facial expression";
+    
+    positivePrompt += tailText;
 
     
-    prompt["1"]["inputs"]["image"]=imgData;
+    prompt["44"]["inputs"]["image"]=imgData;
 
-    prompt["55"]["inputs"]["text_positive"] = positivePrompt;
+    prompt["6"]["inputs"]["text"] = positivePrompt;
+    console.log( prompt["6"]["inputs"]["text"] );
+
     //prompt["55"]["inputs"]["text_negative"] = negtivePrompt;
     //prompt["55"]["inputs"]["style"] = style;
 
-    prompt["45"]["inputs"]["seed"] =  Tool.randomInt();
+    prompt["3"]["inputs"]["seed"] =  Tool.randomInt();
 
     return prompt;
 }
