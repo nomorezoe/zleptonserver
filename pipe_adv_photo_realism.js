40c9ba19b8523c42e7e027d93aa48341f3aff856
nomorezoe <zoewangwork@gmail.com>
2024-04-16 14:29:52 +0800

    update

--- pipe_adv_photo_realism.js
+++ pipe_adv_photo_realism.js
@@ -98,8 +98,9 @@ PipeAdvancePhotoRealism.process = function (imgData, positivePrompt, negtiveProm
     //ch lock
     if (CharacterTool.lockChParams.isLockCharacter) {
         let pair = CharacterTool.characerLockPair;
+        console.log("pair" + pair.length);
         if (pair.length == 0) {
-            pair = [0, 0];
+            pair = [[0, 0]];
         }
         CharacterTool.AddAPerson(prompt, pair, CharacterTool.lockChParams.fullCharacterPath, "1", "53", "192", "69", "68", "194", "223", "4");
     }
